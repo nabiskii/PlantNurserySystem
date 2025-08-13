e882889608ca2d00ccc613626523fba0e69ab7b0
Nabila <nabilazehra27@gmail.com>
2025-08-14 01:02:30 +1000

    Added plant model and crud operations through controller.

---
+++ backend/models/Plant.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const PlantSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Plant name is required'],
+        trim: true
+    },
+    category: {
+        type: String,
+        enum: ['Indoor', 'Outdoor', 'Succulent', 'Flowering', 'Herb', 'Other'],
+        required: [true, 'Category is required']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is required'],
+        min: [0, 'Price must be a positive number']
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageURL: {
+        type: String,
+        required: [true, 'Image URL is required']
+    },
+},
+{
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model('Plant', PlantSchema);
